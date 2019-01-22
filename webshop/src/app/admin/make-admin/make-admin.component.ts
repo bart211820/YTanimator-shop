5a8667a9d0a3e67ec953708838d7b1c39ac8b617
bart211820 <bartlans76@gmail.com>
2019-01-22 10:13:17 +0100

    create new admin
    Now able to make a new admin account

--- webshop/src/app/admin/make-admin/make-admin.component.ts
+++ webshop/src/app/admin/make-admin/make-admin.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import {ApiService} from "../../shared/api.service";
+import {AuthorizationService} from "../../shared/authorization.service";
+import {Router} from "@angular/router";
+import {UserService} from "../../shared/modelsAndTheirServices/user.service";
+import {User} from "../../shared/modelsAndTheirServices/user";
 
 @Component({
   selector: 'app-make-admin',
   templateUrl: './make-admin.component.html',
-  styleUrls: ['./make-admin.component.css']
+  styleUrls: ['./make-admin.component.css'],
+  providers: [
+    ApiService,
+    AuthorizationService,
+    UserService
+  ]
 })
 export class MakeAdminComponent implements OnInit {
 
-  constructor() { }
+  fullName: String;
+  postcode: String;
+  streetnumber: String;
+  email: String;
+  password: String;
+
+  constructor(private api: ApiService, private authService: AuthorizationService, private router: Router, private userService: UserService) { }
 
   ngOnInit() {
+  }
+
+  makeAccount() {
+    const userData = {
+      userID: undefined,
+      fullName: this.fullName,
+      postcode: this.postcode,
+      streetnumber: this.streetnumber,
+      emailAddress: this.email,
+      password: this.password,
+      roles: ["GUEST", "ADMIN"]
+    };
+    const user = new User(userData);
+    this.userService.register(user);
   }
 
 }
